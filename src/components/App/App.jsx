3896d6f5cc3188ee4ddc58b8818bee8d917fdde3
Jeremy Hanson <rockbasser88@hotmail.com>
2023-01-19 16:26:21 -0600

    set up redux and started CustomerForm component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import CustomerForm from '../CustomerForm';
+
 
 function App() {
 
@@ -12,7 +14,7 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <CustomerForm />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// customer info reducer
+const customerInfoList = (
+  state=[
+    {
+      id: 1,
+      customerName: 'Jeremy',
+      streetAddress: '123 My House',
+      city: 'Minneapolis',
+      zip: '55555',
+      type: 'delivery'
+    }
+  ], action
+  ) => {
+  if (action.type === 'ADD_CUSTOMER') {
+    return action.payload;
+  }
+  return state;
+}
+
+// Redux store
+const reduxStore = createStore(
+  combineReducers({
+    customerInfoList
+  }),
+  applyMiddleware(logger)
+)
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
